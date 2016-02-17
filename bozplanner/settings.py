c0894c79180dabbe0296ff6c596e23193ad22c25
Pieter Bos <pieter@vck.utwente.nl>
2016-02-17 12:20:58 +0100

    Move templates to subapps

--- bozplanner/settings.py
+++ bozplanner/settings.py
@@ -45,7 +45,6 @@ ROOT_URLCONF = 'bozplanner.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
