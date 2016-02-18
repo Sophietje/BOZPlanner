09b586383f3c2e7ca18a60ab01c012f112f24fbb
Pieter Bos <pieter@vck.utwente.nl>
2016-02-18 17:14:04 +0100

    Drop permissions from user

--- members/models.py
+++ members/models.py
@@ -1,21 +1,18 @@
-import django.contrib.auth.models
+from django.contrib.auth.models import User, Permission
 from django.db import models
 
-class Person(django.contrib.auth.models.User):
+
+class Person(User):
     """Any user that can log in to BOZPlanner"""
     organization = models.ManyToManyField("Organization", blank=True)
 
     class Meta:
         verbose_name = "person"
-        permissions = (
-            ("admin", "Can do anything"),
-            ("plan", "Can schedule and change meetings"),
-            ("take_minutes", "Can upload minutes"),
-            ("approve_minutes", "Can approve minutes"),
-        )
 
     def __str__(self):
         return self.get_full_name()
+
+
 
 class Organization(models.Model):
     """An organization for which OLC meetings can be planned in BOZPlanner"""
