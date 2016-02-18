725580a49d3cc5846710cbf954d528cdd9777152
Pieter Bos <info@pieterbos.me>
2016-02-18 14:26:52 +0100

    Documentation

--- meetings/managers.py
+++ meetings/managers.py
@@ -2,6 +2,8 @@ import icalendar
 from django.db import models
 
 class MeetingManager(models.Manager):
+    """Manager for the Meeting model that adds a method to retrieve the queryset as an iCalendar"""
+
     def as_icalendar(self):
         """Returns the current queryset as an iCalendar"""
         calendar = icalendar.Calendar()
