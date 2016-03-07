5b6f6978e14a9bf65ad5f4d03e25bd22607b8b1b
Sophie Lathouwers <sophie.lathouwers@gmail.com>
2016-03-07 16:36:52 +0100

    Minutes, belonging to a meeting, can now be uploaded, they are stored in the directory 'media/'

--- meetings/views.py
+++ meetings/views.py
@@ -1,8 +1,10 @@
 from django.core.urlresolvers import reverse_lazy
-from django.views.generic import CreateView, UpdateView, DeleteView, ListView, View
+from django.shortcuts import render, redirect
+from django.views.generic import CreateView, UpdateView, DeleteView, ListView, View, FormView
 from django.http import HttpResponse
 
-from meetings.models import Meeting
+from meetings.models import Meeting, Minutes
+
 
 class MeetingsView(ListView):
     model = Meeting
@@ -25,3 +27,17 @@ class MeetingsIcsView(View):
     def get(self, request):
         calendar = Meeting.objects.as_icalendar()
         return HttpResponse(calendar.to_ical(), content_type="text/calendar")
+
+class MinuteUploadView(View):
+    succes_url = reverse_lazy('meetings')
+    template_name = 'meetings/upload_minutes.html'
+
+    def post(self, request, *args, **kwargs):
+        form = request.POST
+        file = request.FILES['minutes']
+        print (file)
+        meeting = Meeting.objects.get(id=form.get('meeting'))
+        minutes = Minutes.objects.create(file=file, meeting=meeting)
+        minutes.save()
+        return HttpResponse('Minutes have been added.')
+
