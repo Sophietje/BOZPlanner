4e848652f460cdb1284f8c9a5baf15700f9f6b0b
kimberlyh <hengst.kimberly@gmail.com>
2016-03-18 17:19:47 +0100

    Templates for overview mail; send overview mail to people with preferences; show organization in mail content

--- bozplanner/management/commands/overview_mail.py
+++ bozplanner/management/commands/overview_mail.py
@@ -1,8 +1,9 @@
+from django.core.management.base import BaseCommand
 from django.core.mail import EmailMultiAlternatives
-from django.core.management import BaseCommand
-from django.template.loader import get_template
 from django.utils import timezone
+from django.template.loader import get_template
 from meetings.models import Meeting
+from members.models import Organization
 
 
 class Command(BaseCommand):
@@ -11,19 +12,16 @@ class Command(BaseCommand):
         deadline_end = timezone.now().replace(hour=23,minute=59,second=59) + timezone.timedelta(days=7)
         deadline_start = timezone.now().replace(hour=0,minute=0,second=0)
 
-        # TODO: organization en vinkjes fixen
-        # TODO: Define who should be mailed about the secretary missing: people subscribe themselves
-        meetings = [meeting for meeting in Meeting.objects.filter(begin_time__lt=deadline_end, begin_time__gt=deadline_start).order_by('begin_time')]
+        for organization in Organization.objects.all():
+            meetings = [meeting for meeting in Meeting.objects.filter(organization=organization, begin_time__lt=deadline_end, begin_time__gt=deadline_start).order_by('begin_time')]
+            if len(meetings) > 0:
+                mail_context = {'meetings' : meetings, 'organization' : organization}
+                subject = '[BOZPlanner] ['+organization.name+'] Overview meetings this week'
+                text_content = get_template('overview_mail_plain.html').render(context=mail_context)
+                html_content = get_template('overview_mail_html.html').render(context=mail_context)
+                from_email   = 'bozplanner@utwente.nl'
+                to           = organization.pref_overview.values_list('person__email', flat=True)
 
-        if len(meetings) > 0:
-            mail_context = {'meetings' : meetings}
-            subject = "Overview meetings this week"
-            text_content = get_template('overview_mail_plain.html').render(context=mail_context)
-            html_content = get_template('overview_mail_html.html').render(context=mail_context)
-            from_email   = 'bozplanner@utwente.nl'
-            to           = ['hengst.kimberly@gmail.com']
-            mail = EmailMultiAlternatives(subject, text_content, from_email, to)
-            mail.attach_alternative(html_content, "text/html")
-
-        #mail.send()
-        print(mail.message())
+                mail = EmailMultiAlternatives(subject, text_content, from_email, to)
+                mail.attach_alternative(html_content, "text/html")
+                mail.send()
--- bozplanner/management/commands/reminder_mail.py
+++ bozplanner/management/commands/reminder_mail.py
@@ -1,4 +1,4 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from django.core.mail import EmailMultiAlternatives
 from django.utils import timezone
 from django.template.loader import get_template
@@ -12,12 +12,10 @@ class Command(BaseCommand):
         deadline_end = timezone.now().replace(hour=23,minute=59,second=59) + timezone.timedelta(days=7)
         deadline_start = timezone.now().replace(hour=0,minute=0,second=0) + timezone.timedelta(days=7)
 
-        # TODO: specifyen van welke organization de meetings moeten komen
-        # TODO: Define who should be mailed about the secretary missing: people subscribe themselves
         for organization in Organization.objects.all():
             meetings = [meeting for meeting in Meeting.objects.filter(secretary = None, organization=organization, begin_time__lt=deadline_end, begin_time__gt=deadline_start).order_by('begin_time')]
             if len(meetings) > 0:
-                mail_context = {'meetings' : meetings}
+                mail_context = {'meetings' : meetings, 'organization' : organization}
                 if len(meetings) == 1:
                     subject      = '[BOZPlanner] ['+organization.name+'] Secretary required: '+meetings[0].begin_time.strftime('%Y-%m-%d %H:%M')
                 else:
@@ -25,10 +23,8 @@ class Command(BaseCommand):
                 text_content = get_template('reminder_mail_plain.html').render(context=mail_context)
                 html_content = get_template('reminder_mail_html.html').render(context=mail_context)
                 from_email   = 'bozplanner@utwente.nl'
-                #to           = ['hengst.kimberly@gmail.com']
                 to           = organization.pref_reminder.values_list('person__email', flat=True)
 
                 mail = EmailMultiAlternatives(subject, text_content, from_email, to)
                 mail.attach_alternative(html_content, "text/html")
                 mail.send()
-                #print(mail.message())
