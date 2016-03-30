07ee6dab41ecba74f7c3d54c0cc93f306a4796a5
Sophie Lathouwers <sophie.lathouwers@gmail.com>
2016-03-30 15:39:37 +0200

    Added test cases for deleting meetings for admins

--- meetings/tests/test_utils.py
+++ meetings/tests/test_utils.py
@@ -51,15 +51,15 @@ class TestUtils():
         # Get first organization in database, this should be "Test"
         o = Organization.objects.get(pk=1)
         # Add meeting to be able to check the context
-        m = Meeting.objects.create(organization=o, begin_time=datetime(2016, 11, 11, 11, 11, 0), end_time=datetime(2016, 11, 12, 13, 14, 0), place='Zi 0000')
+        m = Meeting.objects.create(organization=o, begin_time=datetime(9999, 11, 11, 11, 11, 0), end_time=datetime(9999, 11, 12, 13, 14, 0), place='Zi 0000')
         m.save()
 
         # Add meeting from new organization
         p = Organization.objects.create(name='Special')
-        m = Meeting.objects.create(organization=p, begin_time=datetime(2016, 11, 12, 14, 15, 0), end_time=datetime(2016, 11, 12, 15, 15, 0), place='Educafé')
+        m = Meeting.objects.create(organization=p, begin_time=datetime(9999, 11, 12, 14, 15, 0), end_time=datetime(9999, 11, 12, 15, 15, 0), place='Educafé')
         m.save()
 
         # Add meeting from suborganization of 'Test'
         q = Organization.objects.create(name='Subtest', parent_organization=o)
-        m = Meeting.objects.create(organization=q, begin_time=datetime(2016, 11, 12, 11, 12, 0), end_time=datetime(2016, 11, 12, 12, 0, 0), place='CR 2D')
+        m = Meeting.objects.create(organization=q, begin_time=datetime(9999, 11, 12, 11, 12, 0), end_time=datetime(9999, 11, 12, 12, 0, 0), place='CR 2D')
         m.save()
