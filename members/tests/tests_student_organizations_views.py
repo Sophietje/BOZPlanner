dbddca9a6caca80167cbf431ec277f4b9397f983
Sophie Lathouwers <sophie.lathouwers@gmail.com>
2016-04-04 10:41:11 +0200

    Changed tests due to other HTTP responses because of changes in permissions

--- members/tests/tests_student_organizations_views.py
+++ members/tests/tests_student_organizations_views.py
@@ -12,13 +12,13 @@ class TestStudentOrganizationsViews(TestCase):
     def test_organizations_list(self):
         resp = self.client.get(reverse('members:organizations'))
         # Student is not allowed to view organizations so should be redirected (302)
-        self.assertEqual(resp.status_code, 302)
+        self.assertEqual(resp.status_code, 403)
 
     def test_organizations_correct_update(self):
         p_orga = Organization.objects.create(name='Parent')
         resp = self.client.post(reverse('members:organization_update', kwargs={'pk': 1}), {'name':'Child', 'parent_organization': p_orga.pk})
 
-        self.assertEqual(resp.status_code, 302)
+        self.assertEqual(resp.status_code, 403)
         # Ensure nothing has been updated (student is not allowed to update organizations)
         self.assertEqual(Organization.objects.get(pk=1).name, 'Test')
         self.assertEqual(Organization.objects.get(pk=1).parent_organization_id, None)
@@ -26,19 +26,19 @@ class TestStudentOrganizationsViews(TestCase):
     def test_organizations_incorrect_update(self):
         # Ensure that a non-existent pk throws a 404
         resp = self.client.post(reverse('members:organization_update', kwargs={'pk': 9999}))
-        self.assertEqual(resp.status_code, 302)
+        self.assertEqual(resp.status_code, 403)
 
         # Post NO data
         resp = self.client.post(reverse('members:organization_update', kwargs={'pk': 1}))
-        self.assertEqual(resp.status_code, 302)
+        self.assertEqual(resp.status_code, 403)
 
         # Post junk data
         resp = self.client.post(reverse('members:organization_update', kwargs={'pk': 1}), {'foo': 'bar'})
-        self.assertEqual(resp.status_code, 302)
+        self.assertEqual(resp.status_code, 403)
 
         # Send non-existent parent-organization pk
         resp = self.client.post(reverse('members:organization_update', kwargs={'pk': 1}), {'parent_organization': 9999})
-        self.assertEqual(resp.status_code, 302)
+        self.assertEqual(resp.status_code, 403)
         self.assertEqual(Organization.objects.get(pk=1).parent_organization, None)
 
     def test_organizations_correct_delete(self):
@@ -47,7 +47,7 @@ class TestStudentOrganizationsViews(TestCase):
         self.assertEqual('Parent', Organization.objects.get(pk=2).name)
         # Delete newly added organization
         resp = self.client.post(reverse('members:organization_delete', kwargs={'pk': orga.pk}))
-        self.assertEqual(resp.status_code, 302)
+        self.assertEqual(resp.status_code, 403)
         # Ensure that organization has NOT been deleted
         self.assertNotEqual(None, Organization.objects.get(pk=2))
         # Delete newly added organization
@@ -56,6 +56,6 @@ class TestStudentOrganizationsViews(TestCase):
     def test_organizations_incorrect_delete(self):
         # Delete non-existing organization
         resp = self.client.post(reverse('members:organization_delete', kwargs={'pk': 9999}))
-        self.assertEqual(resp.status_code, 302)
+        self.assertEqual(resp.status_code, 403)
         # Assert that list of organizations did not change
         self.assertEqual([orga.pk for orga in Organization.objects.all()], [1])
--- members/tests/tests_student_users_views.py
+++ members/tests/tests_student_users_views.py
@@ -13,29 +13,29 @@ class TestStudentUsersViews(TestCase):
     def test_users_list(self):
         # Ensure that students may not view this page, should be redirected (302)
         resp = self.client.get(reverse('members:persons'))
-        self.assertEqual(resp.status_code, 302)
+        self.assertEqual(resp.status_code, 403)
 
     # Student should not be allowed to edit users so should be redirected
     def test_users_update(self):
         # Ensure non-existent pk throws a 302
         resp = self.client.post(reverse('members:person_update', kwargs={'pk': 9999}))
-        self.assertEqual(resp.status_code, 302)
+        self.assertEqual(resp.status_code, 403)
 
         # Post NO data
         user_1 = Person.objects.get(pk=1)
         resp = self.client.post(reverse('members:person_update', kwargs={'pk': 1}))
-        self.assertEqual(resp.status_code, 302)
+        self.assertEqual(resp.status_code, 403)
         # Ensure meeting has not been changed
         self.assertEqual(user_1, Person.objects.get(pk=1))
 
         # Post correct data, user should NOT be updated
         resp = self.client.post(reverse('members:person_update', kwargs={'pk': 1}), {'first_name': 'Tester'})
-        self.assertEqual(resp.status_code, 302)
+        self.assertEqual(resp.status_code, 403)
         self.assertEqual(user_1, Person.objects.get(pk=1))
 
         # Post junk data, user should NOT be updated
         resp = self.client.post(reverse('members:person_update', kwargs={'pk': 1}), {'test': 'onzin'})
-        self.assertEqual(resp.status_code, 302)
+        self.assertEqual(resp.status_code, 403)
         self.assertEqual(user_1, Person.objects.get(pk=1))
 
     def test_users_delete(self):
@@ -43,6 +43,6 @@ class TestStudentUsersViews(TestCase):
         Person.objects.get(pk=1)
         # Ensure that student may not delete a user
         resp = self.client.post(reverse('members:person_delete', kwargs={'pk': 1}))
-        self.assertEqual(resp.status_code, 302)
+        self.assertEqual(resp.status_code, 403)
         # Ensure meeting is not deleted
         Person.objects.get(pk=1)
