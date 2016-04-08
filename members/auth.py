5b31d12bf444057b8d0f72d8aef8230e7fffb413
Pieter Bos <pieter@vck.utwente.nl>
2016-04-08 15:28:17 +0200

    Remove custom auth backend

--- members/auth.py
+++ members/auth.py
@@ -18,21 +18,3 @@ csrf_exempt = method_decorator(csrf.csrf_exempt, name='dispatch')
 def permission_required(*args, **kwargs):
     kwargs['raise_exception'] = True
     return method_decorator(decorators.permission_required(*args, **kwargs), name='dispatch')
-
-
-class SAML2Backend:
-    def authenticate(self, session_info, attribute_mapping, create_unknown_user):
-        # Temporary hack to get the s-nummer out of the name_id (s1234567@utwente.nl)
-
-        username = session_info['name_id'].text.split("@")[0]
-
-        try:
-            return Person.objects.get(username=username)
-        except Person.DoesNotExist:
-            raise PermissionDenied
-
-    def get_user(self, user_id):
-        try:
-            return Person.objects.get(pk=user_id)
-        except Person.DoesNotExist:
-            return None
